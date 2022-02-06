01aa4922fa9b380c929ec496c165256fff622a18
Alex Giovani Hirsch <alexgiovani35@gmail.com>
2022-02-05 23:37:59 -0300

    novos testes com manipulação big data

--- python/testes/biblioteca-pandas/teste_dados.py
+++ python/testes/biblioteca-pandas/teste_dados.py
@@ -1,21 +1,19 @@
-arquivo = r"C:\Games\parrot\dicionarios\Nova pasta (2)\Top2Billion-probable-v2.txt"
-
-
 def getlines(arquivo, linha_inicio, quatidade_linha, encoding='utf8'):
-    #-> Permite buscar em um arquivo de texto gigante de até mais de 20gb sem exceder a memoria ram, e armazenar uma certa quantidade de dados #por linhas em forma de uma lista.
+    """-> Permite buscar em um arquivo de texto gigante de até mais de 20gb sem exceder a memoria ram,
+    e armazenar uma certa quantidade de dados #por linhas em forma de uma lista.
+    arquivo(str) = diretório onde o arquivo se encontra
+    linha_inicio(int) = o numero da linha onde vai iniciar a captura de dados.
+    quantidade_linha(int) = quantidade de dados por linha que vai ser capturado após o linha_inicio.
+    encoding(str) = codificação ex: 'utf8' etc.
+    return(list) = [dados]"""
 
-    #arquivo(str) = diretório onde o arquivo se encontra ex: r"C:\user\usuario\desktop\arquivo.txt"
-    #linha_inicio(int) = o numero da linha onde vai iniciar a captura de dados.
-    #quantidade_linha(int) = quantidade de dados por linha que vai ser capturado após o linha_inicio.
-    #encoding(str) = codificação ex: 'utf8' etc.
-
-    #return(list) = [dados]
+    from tqdm import tqdm
 
     resultado = list()
     with open(arquivo, encoding=encoding) as dicionario:
         flag = False
         end_line = linha_inicio + (quatidade_linha - 1)
-        for i, line in enumerate(dicionario):
+        for i, line in tqdm(enumerate(dicionario), 'Captando linhas', unit='lines'):
             if i == linha_inicio:
                 flag = True
             if flag:
@@ -24,5 +22,30 @@ def getlines(arquivo, linha_inicio, quatidade_linha, encoding='utf8'):
                     return resultado
 
 
-resultado = getlines(arquivo, 10000, 100)
-print(resultado)
+def escrever_dados(arquivo, lista_dados, encoding='utf8'):
+    """ Escreve dados em um novo arquivo de texto
+    arquivo(str) = diretório onde o arquivo se encontra
+    lista_dados(list) = lista que será colocada no arquivo
+    encoding(str) = codificação ex: 'utf8' etc."""
+
+    from tqdm import tqdm
+    from time import sleep
+
+    try:
+        with open(arquivo, 'a', encoding=encoding) as f_obj:
+            for d in tqdm(lista_dados, 'Escrevendo Dados', unit='lines', colour='green'):
+                f_obj.write(d + '\n')      
+    except FileNotFoundError:
+        print(f'Arquivo ({arquivo}) não encontrado, criando arquivo...')
+        sleep(1.5)
+        print(f'Arquivo ({arquivo}) criado com sucesso!')
+        with open(arquivo, 'w', encoding='utf8') as f_obj:
+            for d in tqdm(lista_dados, 'Escrevendo Dados', unit='lines', colour='green'):
+                f_obj.write(d + '\n')
+        
+
+arquivo = r"C:\Games\parrot\dicionarios\Nova pasta (2)\Top2Billion-probable-v2.txt"
+resultado = getlines(arquivo, 200, 10, 'ansi')
+escrever_dados('teste.txt', resultado)
+
+print('\nfim\n')
