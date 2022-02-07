2a10c2f424e473a0aff1b03b2be41c144bc3a0ad
Alex Giovani Hirsch <alexgiovani35@gmail.com>
2022-02-06 22:52:08 -0300

    update

--- python/testes/biblioteca-pandas/teste_dados.py
+++ python/testes/biblioteca-pandas/teste_dados.py
@@ -1,31 +1,40 @@
 def getlines(arquivo, linha_inicio, quatidade_linha, encoding='utf8'):
     """-> Permite buscar em um arquivo de texto gigante de até mais de 20gb sem exceder a memoria ram,
     e armazenar uma certa quantidade de dados #por linhas em forma de uma lista.
+    
     arquivo(str) = diretório onde o arquivo se encontra
-    linha_inicio(int) = o numero da linha onde vai iniciar a captura de dados.
-    quantidade_linha(int) = quantidade de dados por linha que vai ser capturado após o linha_inicio.
-    encoding(str) = codificação ex: 'utf8' etc.
-    return(list) = [dados]"""
 
-    from tqdm import tqdm
+    linha_inicio(int) = o numero da linha onde vai iniciar a captura de dados.
+    
+    quantidade_linha(int) = quantidade de dados por linha que vai ser capturado após o linha_inicio.
+    
+    encoding(str) = codificação ex: 'utf8' etc.
+    
+    return(list) = [dados] """
 
     resultado = list()
+    print('Procurando linha...')
     with open(arquivo, encoding=encoding) as dicionario:
         flag = False
         end_line = linha_inicio + (quatidade_linha - 1)
-        for i, line in tqdm(enumerate(dicionario), 'Captando linhas', unit='lines'):
+        for i, line in enumerate(dicionario):
             if i == linha_inicio:
+                print('Capturando linhas, aguarde...')
                 flag = True
             if flag:
                 resultado.append(line.strip())
                 if i == end_line:
+                    print('Captura concluida.')
                     return resultado
 
 
 def escrever_dados(arquivo, lista_dados, encoding='utf8'):
     """ Escreve dados em um novo arquivo de texto
+    
     arquivo(str) = diretório onde o arquivo se encontra
+    
     lista_dados(list) = lista que será colocada no arquivo
+    
     encoding(str) = codificação ex: 'utf8' etc."""
 
     from tqdm import tqdm
@@ -62,6 +71,12 @@ def count_lines(arquivo, numb_linhas=0, encoding='utf8'):
 
 
 def data_filter(data_list, totchar_numb=8):
+    """ - filtra palavras por numero de caracteres -
+    data_list(list) = recebe lista que deseja filtrar
+    
+    totchar_numb(int) = numero minimo de caracteres
+    
+    return = retorna lista(list) com o resultado """
     
     from tqdm import tqdm
 
@@ -77,15 +92,18 @@ def data_filter(data_list, totchar_numb=8):
             result.append(data)
     return result
 
-arquivo = r"C:\Games\parrot\dicionarios\Nova pasta (2)\Top2Billion-probable-v2.txt"
 
+# - programa principal -
 from tqdm import tqdm
+
+arquivo = r"C:\Games\parrot\dicionarios\Nova pasta (2)\Top2Billion-probable-v2.txt"
 
 dado = 10000000
 n = count_lines(arquivo, 1973218846, 'ansi')
-n1 = 111 * 10000000
+n1 = 113 * 10000000
 vezes = 197 - 111
-for c in tqdm(range(vezes), 'DADOS', unit='packets', colour='cyan'):
+
+for c in tqdm(range(vezes), 'Progresso Geral', unit='packets', colour='cyan'):
     if n == 0:
         break
     resultado = getlines(arquivo, n1, dado, 'ansi')
@@ -95,4 +113,5 @@ for c in tqdm(range(vezes), 'DADOS', unit='packets', colour='cyan'):
     filtrado.clear
     n -= dado
     n1 += dado
+
 print('fim')
