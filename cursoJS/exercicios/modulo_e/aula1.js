ccebb6106cfdea977f21b2a68014c9279692d148
Alex Giovani Hirsch <alexgiovani35@gmail.com>
2022-11-22 00:35:35 -0300

    aula 1 mod e finalizada

--- cursoJS/exercicios/modulo_e/aula1.js
+++ cursoJS/exercicios/modulo_e/aula1.js
@@ -1,5 +1,13 @@
+//estrutura de repetição com teste no inicio
 var c = 1;
 while (c <= 6) {
     console.log(`passo ${c}`);
-    
+    c++
 }
+
+//estrutura de repetição com teste no final
+var a = 1;
+do {
+    console.log(`a = ${a}`);
+    a++
+} while (a <= 6);
