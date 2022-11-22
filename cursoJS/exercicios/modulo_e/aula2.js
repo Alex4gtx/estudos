abf9867f168a9c9a85786b83bab6b222d8c8c539
Alex Giovani Hirsch <alexgiovani35@gmail.com>
2022-11-22 01:36:31 -0300

    repetição for

--- cursoJS/exercicios/modulo_e/aula2.js
+++ cursoJS/exercicios/modulo_e/aula2.js
@@ -1,1 +1,24 @@
-console.log(123);
+//estrutura de repetição com variavel de controle
+// inicialização / teste lógico / incremento
+
+// for
+for (let i = 1;i <= 6;i++) {
+    console.log(`o i é igual a ${i}`);
+}
+
+// for each = para cada elemento
+let array = ['alex', 234, 'mila'];
+array.forEach(element => {
+    console.log(element);
+});
+
+let obj1 = {nome: 'alex', sobrenome: 'hirsch', idade: 25, tamanho: 1.83}
+let arr = [1,2,3,4,5,6,'jjj']
+
+for (const key in obj1) {
+    console.log(key);
+}
+
+for (const key of arr) {
+    console.log(key);
+}
