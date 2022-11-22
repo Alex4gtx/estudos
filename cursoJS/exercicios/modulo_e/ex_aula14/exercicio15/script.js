7d82847a6773086ff2219d4960d893d8df716a3e
Alex Giovani Hirsch <alexgiovani35@gmail.com>
2022-11-22 04:16:18 -0300

    vai ficar para amanhã kkkkk

--- cursoJS/exercicios/modulo_e/ex_aula14/exercicio15/script.js
+++ cursoJS/exercicios/modulo_e/ex_aula14/exercicio15/script.js
@@ -1,11 +1,16 @@
-let resultado = document.getElementById("iresp");
-
-let inicio = Number(document.getElementById("iinicio").value);
-let fim = Number(document.getElementById("ifim").value);
-let passo = Number(document.getElementById("ipasso").value);
-
+//precisa de atenção
 function verificar() {
-    for (inicio; inicio <= fim; inicio += passo) {
-        resultado.innerHTML += `${inicio} => `;
+    let resultado = document.getElementById("iresp");
+    let inicio = document.getElementById("iinicio");
+    let fim = document.getElementById("ifim");
+    let passo = document.getElementById("ipasso");
+    let i = 1;
+    let f = fim.value;
+    let p = passo.value;
+
+    resultado.innerText = '';
+    for (i; i <= f; i += p) {
+        resultado.innerText += `${i} => `;
     }
+    resultado.innerText += 'bandeirinha :3';
 }
