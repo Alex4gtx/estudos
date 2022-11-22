73b372c01e1a86131ccffd25c37a75632cfce118
Alex Giovani Hirsch <alexgiovani35@gmail.com>
2022-11-22 18:46:00 -0300

    exercicio concluido com sucesso!

--- cursoJS/exercicios/modulo_e/ex_aula14/exercicio15/script.js
+++ cursoJS/exercicios/modulo_e/ex_aula14/exercicio15/script.js
@@ -1,16 +1,30 @@
 //precisa de atenção
 function verificar() {
-    let resultado = document.getElementById("iresp");
+    let resultado = document.querySelector("div#iresp>p"); //onde será mostrado o resultado
     let inicio = document.getElementById("iinicio");
     let fim = document.getElementById("ifim");
     let passo = document.getElementById("ipasso");
-    let i = 1;
-    let f = fim.value;
-    let p = passo.value;
+    
+    //variaveis transformados em numeros
+    let i = Number(inicio.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
 
-    resultado.innerText = '';
-    for (i; i <= f; i += p) {
-        resultado.innerText += `${i} => `;
+    if (i === null || i == '') {
+        window.alert('insira um numero de inicio!');
+    } else if (f === null || f == '') {
+        window.alert('insira um numero de fim!');
+    } else if (i >= f) {
+        window.alert('o valor do inicio não pode ser maior ou igual ao valor do fim!');
+    } else {
+        if (p == 0 || p ==  null) {
+            window.alert('[AVISO] O passo não pode ser igual a 0, foi considerado automátimacente o passo igual a 1.');
+            p = 1;
+        }
+        resultado.innerText = '';
+        for (i; i <= f; i += p) {
+            resultado.innerHTML += `${i} &#128073; `;
+        }
+        resultado.innerHTML += ' &#127988;';
     }
-    resultado.innerText += 'bandeirinha :3';
 }
